44514ddb501d8d622d8b800646a4c99dd747ac2c
mzfirozuddin <mz.firozuddin@gmail.com>
2024-05-08 00:23:27 +0530

    add convertPemToJwks and availbale jwks for public

---
+++ scripts/convertPemToJwk.mjs
@@ -0,0 +1,10 @@
+import fs from "fs";
+import rsaPemToJwk from "rsa-pem-to-jwk";
+
+const privateKey = fs.readFileSync("./certs/private.pem");
+
+const jwk = rsaPemToJwk(privateKey, { use: "sig" }, "public");
+
+// console.log(jwk);
+// eslint-disable-next-line no-console, no-undef
+console.log(JSON.stringify(jwk));
--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import authRouter from "./routes/auth";
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.json());
 
@@ -19,8 +20,9 @@ app.use("/auth", authRouter);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    // console.log(err);
     logger.error(err.message);
-    const statusCode = err.statusCode || 500;
+    const statusCode = err.statusCode || err.status || 500;
     res.status(statusCode).json({
         errors: [
             {
