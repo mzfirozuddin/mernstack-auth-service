2f48b3e91560fe188126a74fb689b3282ea80b93
mzfirozuddin <mz.firozuddin@gmail.com>
2024-05-26 18:02:30 +0530

    add test for create a tenant in DB

--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 import { AppDataSource } from "../../src/config/data-source";
 import request from "supertest";
 import app from "../../src/app";
+import { Tenant } from "../../src/entity/Tenant";
 
 describe("POST /tenants", () => {
     let connection: DataSource;
@@ -34,6 +35,22 @@ describe("POST /tenants", () => {
                 .send(tenantData);
 
             expect(response.statusCode).toBe(201);
+        });
+
+        it("should create a tenent in the database", async () => {
+            const tenantData = {
+                name: "Tenant name",
+                address: "Tenant address",
+            };
+
+            await request(app).post("/tenants").send(tenantData);
+
+            const tenentRepository = connection.getRepository(Tenant);
+            const tenants = await tenentRepository.find();
+
+            expect(tenants).toHaveLength(1);
+            expect(tenants[0].name).toBe(tenantData.name);
+            expect(tenants[0].address).toBe(tenantData.address);
         });
     });
 });
