893dc8cbec47300dad2f14a8721c24dc7967e266
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-05 00:10:35 +0530

    add update tenant endpoint

--- src/controllers/TenantController.ts
+++ src/controllers/TenantController.ts
@@ -3,6 +3,7 @@ import { TenantService } from "../services/TenantService";
 import { CreateTenantRequest } from "../types";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
+import createHttpError from "http-errors";
 
 export class TenantController {
     constructor(
@@ -25,6 +26,36 @@ export class TenantController {
             this.logger.info("Tenant has been created.", { id: tenant.id });
 
             res.status(201).json({ id: tenant.id });
+        } catch (err) {
+            return next(err);
+        }
+    }
+
+    async update(req: CreateTenantRequest, res: Response, next: NextFunction) {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            res.status(400).json({ errors: result.array() });
+        }
+
+        const { name, address } = req.body;
+        const tenantId = req.params.id;
+
+        if (isNaN(Number(tenantId))) {
+            next(createHttpError(400, "Invalid url params!"));
+            return;
+        }
+
+        this.logger.debug("Request for updating a tenant.", req.body);
+
+        try {
+            await this.tenantService.update(Number(tenantId), {
+                name,
+                address,
+            });
+
+            this.logger.info("Tenant has been updated.", { id: tenantId });
+
+            return res.status(200).json({ id: Number(tenantId) });
         } catch (err) {
             return next(err);
         }
