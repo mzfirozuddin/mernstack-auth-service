b4360492d58a30efab0f816d0f9ea685970613b5
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-07 00:46:21 +0530

    add routes for getAll, getOne, delete tenant

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -22,4 +22,13 @@ router.patch("/:id", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
     tenantController.update(req, res, next),
 );
 
+router.get("/", (req, res, next) => tenantController.getAll(req, res, next));
+router.get("/:id", (req, res, next) => tenantController.getOne(req, res, next));
+router.delete(
+    "/:id",
+    authenticate,
+    canAccess([Roles.ADMIN]),
+    (req, res, next) => tenantController.destroy(req, res, next),
+);
+
 export default router;
