cd2396ffd6f70db7803f3aaffd9a350eec50168e
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-09 12:41:25 +0530

    add update user endpoint and validate create user req

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,23 @@
 import { NextFunction, Response } from "express";
 import { UserService } from "../services/UserService";
-import { CreateUserRequest } from "../types";
+import { CreateUserRequest, UpdateUserRequest } from "../types";
 import { Roles } from "../constants";
+import { validationResult } from "express-validator";
+import { Logger } from "winston";
+import createHttpError from "http-errors";
 
 export class UserController {
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private logger: Logger,
+    ) {}
 
     async create(req: CreateUserRequest, res: Response, next: NextFunction) {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() });
+        }
+
         const { firstName, lastName, email, password } = req.body;
         try {
             const user = await this.userService.create({
@@ -17,7 +28,42 @@ export class UserController {
                 role: Roles.MANAGER,
             });
 
+            this.logger.info("User has been created", { id: user.id });
+
             res.status(201).json({ id: user.id });
+        } catch (err) {
+            return next(err);
+        }
+    }
+
+    async update(req: UpdateUserRequest, res: Response, next: NextFunction) {
+        // In our project: We are not allowing user to change the email id since it is used as username
+        // In our project: We are not allowing admin user to change others password
+
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() });
+        }
+
+        const { firstName, lastName, role } = req.body;
+        const userId = req.params.id;
+
+        if (isNaN(Number(userId))) {
+            next(createHttpError(400, "Invalid user params!"));
+        }
+
+        this.logger.debug("Request for updating a user", req.body);
+
+        try {
+            await this.userService.update(Number(userId), {
+                firstName,
+                lastName,
+                role,
+            });
+
+            this.logger.info("User has been updated", { id: userId });
+
+            res.status(200).json({ id: Number(userId) });
         } catch (err) {
             return next(err);
         }
